b8c6a626e84968a170a53cb2821b5965fc94c524
Simon Kovtyk <privat@kovtyk.com>
2024-10-23 09:54:51 +0200

    feat(wezterm): added support for split manipulation

--- shells/wezterm/mappings.lua
+++ shells/wezterm/mappings.lua
@@ -7,7 +7,42 @@ M.keys = {
 		key = "V",
 		mods = "CTRL",
 		action = wezterm.action.PasteFrom "Clipboard"
-	}
+	},
+  {
+    key = "v",
+    mods = "ALT",
+    action = wezterm.action.SplitHorizontal {
+      domain = "CurrentPaneDomain"
+    }
+  },
+  {
+    key = "h",
+    mods = "ALT",
+    action = wezterm.action.SplitVertical {
+      domain = "CurrentPaneDomain"
+    }
+  },
+  {
+    key = "q",
+    mods = "ALT",
+    action = wezterm.action.CloseCurrentPane {
+      confirm = true
+    }
+  },
+  {
+    key = "t",
+    mods = "ALT",
+    action = wezterm.action.CloseCurrentTab {
+      confirm = true
+    }
+  },
+  {
+    key = "s",
+    mods = "ALT",
+    action = wezterm.action.PaneSelect {
+      alphabet = "1234567890"
+    }
+  }
 }
 
 return M
