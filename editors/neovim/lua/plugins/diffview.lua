0378d420b0f299d2407d8f6db0c866accee3640b
Simon <privat@kovtyk.com>
2024-10-25 20:47:09 +0200

    feat(diffview): optimized settings

--- editors/neovim/lua/plugins/diffview.lua
+++ editors/neovim/lua/plugins/diffview.lua
@@ -1,4 +1,19 @@
-local options = {}
+local options = {
+  show_help_hints = false,
+  file_panel = {
+    tree_options = {
+      flatten_dirs = false
+    },
+    win_config = {
+      position = "right",
+      width = 40
+    }
+  },
+  signs = {
+    fold_closed = "",
+    fold_open = ""
+  }
+}
 
 return {
   "sindrets/diffview.nvim",
