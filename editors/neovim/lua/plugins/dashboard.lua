2f1001a53025304d6463e255ab993e6612d87266
Simon <privat@kovtyk.com>
2024-10-27 20:33:07 +0100

    feat(neovim): improved dashboard

--- editors/neovim/lua/plugins/dashboard.lua
+++ editors/neovim/lua/plugins/dashboard.lua
@@ -10,25 +10,30 @@ local logo = [[
       ░   ░         ░       ░ ░           ░ ░  ░       ░ ░        ░  
                                                                  ░   
 ]]
-
-logo = string.rep("\n", 8) .. logo .. "\n\n"
+logo = logo .. string.rep("\n", 2)
 
 local version_info = vim.version()
+
+logo = logo .. "NeoVim " .. version_info.major .. "." .. version_info.minor .. "." .. version_info.patch .. " Custom IDE"
+
+logo = string.rep("\n", 14) .. logo .. string.rep("\n", 3)
+logo = vim.split(logo, "\n")
 
 local options = {
   theme = "doom",
   disable_move = true,
   shortcut_type = "letter",
   hide = {
-    statusline = true,    -- hide statusline default is true
-    tabline = true,       -- hide the tabline
-    winbar = true        -- hide winbar
+    statusline = true,
+    tabline = true,
+    winbar = true
   },
   config = {
-    header = vim.split(logo, "\n"),
+    header = logo,
     center = {
       {
-        desc = "NeoVim Custom " .. version_info.major .. "." .. version_info.minor .. "." .. version_info.patch,
+        desc = "Project",
+        desc_hl = "DashboardFooter"
       },
       {
         icon = " ",
@@ -38,10 +43,49 @@ local options = {
         key_format = "[%s]"
       },
       {
+        icon = " ",
+        desc = "Explore files",
+        action = "Neotree",
+        key = "w",
+        key_format = "[%s]"
+      },
+      {
+        icon = " ",
+        desc = "Search files",
+        action = "Telescope",
+        key = "f",
+        key_format = "[%s]"
+      },
+      {
         icon = " ",
         desc = "Quit",
         action = "qa",
         key = "q",
+        key_format = "[%s]"
+      },
+      {
+        desc = "More options",
+        desc_hl = "DashboardFooter"
+      },
+      {
+        icon = " ",
+        desc = "LSPs",
+        action = "Mason",
+        key = "l",
+        key_format = "[%s]"
+      },
+      {
+        icon = "󰐱 ",
+        desc = "Plugins",
+        action = "Lazy",
+        key = "p",
+        key_format = "[%s]"
+      },
+      {
+        icon = "󱝀 ",
+        desc = "Check health",
+        action = "checkhealth",
+        key = "h",
         key_format = "[%s]"
       }
     }
