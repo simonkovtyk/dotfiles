42ab20eaecb1c3198b5848dd02c5cfa3086039bc
Simon <privat@kovtyk.com>
2024-11-03 00:08:20 +0100

    fix(fish): slow startup

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -1,7 +1,12 @@
-set -x TERM xterm-256color
-set -x TERMINFO ~/.terminfo
-set LS_COLORS "*.desktop=01;4:di=39;44"
+if not set -q FISH_INIT
+  set -x FISH_INIT 1
 
-if type neofetch >/dev/null 2>&1
-  neofetch
+  set -x TERM xterm-256color
+  set -x TERMINFO ~/.terminfo
+  set -x LS_COLORS "*.desktop=01;4:di=39;44"
+
+  if type neofetch >/dev/null 2>&1
+    neofetch
+  end
 end
+
