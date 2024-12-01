58173a58a7858f5e02018dcfc3a741e9996a18bf
Simon <privat@kovtyk.com>
2024-12-01 17:30:04 +0100

    feat(fish): added vi alias for nvim

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -5,6 +5,8 @@ if not set -q FISH_INIT
   set -x TERMINFO ~/.terminfo
   set -x LS_COLORS "*.desktop=01;4:di=39;44"
 
+  alias vi="nvim"
+
   if type neofetch >/dev/null 2>&1
     echo
     neofetch
