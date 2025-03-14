5cc8c3d4425c288e1cbd5e9292ffb0859eff5017
Simon <simon@kovtyk.com>
2025-03-15 00:36:31 +0100

    feat(wezterm): changed font to ibm plex mono and added font presets

--- terms/wezterm/typography.lua
+++ terms/wezterm/typography.lua
@@ -2,7 +2,11 @@ local wezterm = require "wezterm"
 
 local M = {}
 
-M.font = wezterm.font "FiraCode Nerd Font"
+M.font = wezterm.font "BlexMono Nerd Font"
+-- M.font = wezterm.font "FiraCode Nerd Font"
+-- M.font = wezterm.font "Hack Nerd Font"
+-- M.font = wezterm.font "GeistMono NF"
+-- M.font = wezterm.font "JetBrainsMono NF"
 M.font_size = 12.0
 M.harfbuzz_features = {
 	"calt=0",
