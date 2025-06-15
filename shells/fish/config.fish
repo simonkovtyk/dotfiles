541fb4c76cac822dfe4f4ca9a6ef5079163a5e9b
Simon <simon@kovtyk.com>
2025-06-15 16:44:05 +0200

    fix(lsd): enforce white colors

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -2,9 +2,10 @@ if status is-interactive
 	set fish_greeting
 
 	if not set -q FISH_INIT
-	  set -gx FISH_INIT 1
 	  set -gx TERM xterm-256color
 	  set -gx TERMINFO ~/.terminfo
+    set -gx LS_COLORS "di=37:ln=37:so=37:pi=37:ex=37:bd=37:cd=37:su=37:sg=37:tw=37:ow=37:or=37:mi=37"
+	  set -gx FISH_INIT 1
 	end
 
   zoxide init fish | source
@@ -15,6 +16,4 @@ if status is-interactive
     starship init fish | source
     fastfetch
   end
-
 end
-
