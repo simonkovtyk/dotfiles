a92f65f8de9137901ae29ae1f0e2edc4bf39d8d3
Simon <simon@kovtyk.com>
2025-07-24 19:14:45 +0200

    fix(fish): override alacritty terminfo

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,7 @@ if status is-interactive
 	set fish_greeting
 
 	if not set -q FISH_INIT
-	  set -gx TERM xterm-256color
-	  set -gx TERMINFO ~/.terminfo
+	  set -gx TERM alacritty
     set -gx LS_COLORS "di=37:ln=37:so=37:pi=37:ex=37:bd=37:cd=37:su=37:sg=37:tw=37:ow=37:or=37:mi=37"
 	  set -gx FISH_INIT 1
 	end
