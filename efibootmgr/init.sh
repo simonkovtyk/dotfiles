0c3b68606f0e6723e4f81c4ad19921cf949d23d8
Simon <simon@kovtyk.com>
2025-07-26 16:44:01 +0200

    feat(efibootmgr): added

---
+++ efibootmgr/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+efibootmgr --create --disk /dev/nvme1n1 --part 1 --label "arch linux" --loader /vmlinuz-linux --unicode "root=UUID=f0d8f2ad-f719-4ebc-800c-b6213301d88b rw initrd=\initramfs-linux.img loglevel=2 quiet vt.global_cursor_default=0"
