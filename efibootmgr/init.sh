3c9267d80a6cea382b85677e40d5cc6dc489f17c
Simon <simon@kovtyk.com>
2025-07-26 17:35:13 +0200

    feat(efibootmgr): add kernel param to supress systemd tty writes

--- efibootmgr/init.sh
+++ efibootmgr/init.sh
@@ -5,4 +5,4 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-efibootmgr --create --disk /dev/nvme1n1 --part 1 --label "arch linux" --loader /vmlinuz-linux --unicode "root=UUID=f0d8f2ad-f719-4ebc-800c-b6213301d88b rw initrd=\initramfs-linux.img loglevel=2 quiet vt.global_cursor_default=0"
+efibootmgr --create --disk /dev/nvme2n1 --part 1 --label "arch linux" --loader /vmlinuz-linux --unicode "root=UUID=f0d8f2ad-f719-4ebc-800c-b6213301d88b rw initrd=\initramfs-linux.img loglevel=1 quiet vt.global_cursor_default=0 vt.auto=0 systemd.show_status=0"
