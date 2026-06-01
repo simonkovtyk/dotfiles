05e4b461695dadfe2cd1f822602d82ac51d6ea66
Simon <simon@kovtyk.com>
2026-06-01 19:39:06 +0200

    feat: various updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,30 @@
 if status is-interactive
-	set fish_greeting
+  set fish_greeting
 
-	if not set -q FISH_INIT
-    set -gx EDITOR nvim
+  if not set -q FISH_INIT
+    set -gx EDITOR helix
     set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
     set -gx LS_COLORS "di=37:ln=37:so=37:pi=37:ex=37:bd=37:cd=37:su=37:sg=37:tw=37:ow=37:or=37:mi=37"
-	  set -gx FISH_INIT 1
-	end
+    set -gx FISH_INIT 1
+  end
+
+  function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+
+    if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+      builtin cd -- "$cwd"
+    end
+
+    rm -f -- "$tmp"
+  end
 
   zoxide init fish | source
-	alias vi="nvim"
   alias ls="lsd -A"
   alias la="lsd -l"
   alias lt="lsd --tree"
+  alias hx="helix"
+  alias kssh="kitty +kitten ssh"
 
   if test "$TERMINAL_EMULATION" = "true"
     starship init fish | source
@@ -26,3 +38,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# Created by `pipx` on 2026-01-17 17:43:26
+set PATH $PATH /home/simonkov/.local/bin
---
+++ waybar/scripts/music.sh
@@ -0,0 +1,1 @@
+playerctl metadata --format "{{title}} - {{artist}}"
